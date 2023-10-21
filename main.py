75fa511961c08ac3e318b704fcdb7796080042bd
MahtabSinghAujla <125728731+MahtabSinghAujla@users.noreply.github.com>
2023-10-22 10:33:32 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ class Atm () :
     def __init__ (self,cardNum,pin,bal) :
         self.cardNum=cardNum
         self.pin=pin
-        self.bal=1000
+        self.bal=bal
     
     def balanceWD (self,num) :
         self.bal=self.bal+num
         print('Balance has been changed by', num)
     
-    def checkBal (self,bal) :
+    def checkBal (self) :
         print(self.bal)
     
 user_0=Atm(0,'n20q9wufsdvniqiwa345qtw',1000000000000)
+user_0.balanceWD(500)
+user_0.checkBal()
